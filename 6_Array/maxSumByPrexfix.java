24d09f7293337ba85ef1653f50f3d8113a460b4a
Tirtha Ghosh <ghoshtirtha1234@gmail.com>
2025-06-29 11:41:31 +0530

    Add new file

---
+++ 6_Array/maxSumByPrexfix.java
@@ -0,0 +1,28 @@
+public class maxSumByPrexfix {
+  public static void prefixSum(int arr[]){
+    int currSum=0;
+    int maxSum= Integer.MIN_VALUE;
+    int prefix[]=new int[arr.length];
+    prefix[0]=arr[0];
+
+    for(int i=1;i<prefix.length;i++){
+      prefix[i]=prefix[i-1]+arr[i];
+    }
+    for(int i=0;i<arr.length;i++){
+      int start=i;
+      for(int j=0;j<arr.length;j++){
+        int end=j;
+       currSum= start==0?prefix[end]:prefix[end]-prefix[start-1];
+       if(maxSum<currSum){
+        maxSum=currSum;
+       }
+
+      }
+    }
+    System.out.println("The sum of the sub array => "+maxSum);
+  }
+  public static void main(String[] args) {
+    int arr[]={1,2,3,4,5};
+    prefixSum(arr);
+  }
+}
