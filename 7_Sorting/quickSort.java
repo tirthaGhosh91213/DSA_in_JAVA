f5a31484ece96244dd25b271919ea3b7bee88a66
Tirtha Ghosh <ghoshtirtha1234@gmail.com>
2025-07-02 23:40:45 +0530

    Modify

--- 7_Sorting/quickSort.java
+++ 7_Sorting/quickSort.java
@@ -6,25 +6,25 @@ public class quickSort {
 
     public static void QuickSort(int[] arr, int low, int high) {
         if (low < high) {
-            int pi = partition(arr, low, high);  // partition index
-            QuickSort(arr, low, pi - 1);         // left side
-            QuickSort(arr, pi + 1, high);        // right side
+            int pi = partition(arr, low, high);  
+            QuickSort(arr, low, pi - 1);        
+            QuickSort(arr, pi + 1, high);       
         }
     }
 
     private static int partition(int[] arr, int low, int high) {
-        int pivot = arr[high];  // choose last element as pivot
-        int i = low - 1;        // index of smaller element
+        int pivot = arr[high]; 
+        int i = low - 1;        
 
         for (int j = low; j < high; j++) {
             if (arr[j] < pivot) {
                 i++;
-                // swap arr[i] and arr[j]
+                
                 int temp = arr[i]; arr[i] = arr[j]; arr[j] = temp;
             }
         }
 
-        // swap arr[i+1] and arr[high] (pivot)
+        
         int temp = arr[i + 1]; arr[i + 1] = arr[high]; arr[high] = temp;
 
         return i + 1;
