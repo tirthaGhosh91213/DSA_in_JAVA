5b4d90c6f43ff828323640e8bd094f31bb85bb85
Tirtha Ghosh <ghoshtirtha1234@gmail.com>
2025-07-03 23:33:40 +0530

    Modify

--- 6_Array/basic.java
+++ 6_Array/basic.java
@@ -7,12 +7,12 @@ public class basic {
      marks[0]=sc.nextInt();
      marks[1]=sc.nextInt();
      marks[2]=sc.nextInt();
-     System.out.println("The marks obtained by the student in math "+marks[0]);
-     System.out.println("The marks obtained by the student in physics  "+marks[1]);
-     System.out.println("The marks obtained by the student in chemistry "+marks[2]);
+     System.out.println("The marks obtained by the student in math :-"+marks[0]);
+     System.out.println("The marks obtained by the student in physics :- "+marks[1]);
+     System.out.println("The marks obtained by the student in chemistry :- "+marks[2]);
      marks[0]=marks[0]+5;
      System.out.println("In matha "+marks[0]);
-     System.out.println("Length of the array is "+marks.length);
+     System.out.println("Length of the array is :- "+marks.length);
 
  
   }
