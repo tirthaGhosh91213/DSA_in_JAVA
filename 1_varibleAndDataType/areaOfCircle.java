deda9f41650de299c1fa2f0fc7d1b4e7d4c6b37d
Tirtha Ghosh <ghoshtirtha1234@gmail.com>
2025-07-03 23:31:34 +0530

    Modify

--- 1_varibleAndDataType/areaOfCircle.java
+++ 1_varibleAndDataType/areaOfCircle.java
@@ -7,6 +7,6 @@ public class areaOfCircle{
       Scanner sc=new Scanner(System.in);
       float rad=sc.nextFloat();
       float area =3.14f*rad*rad;
-      System.out.println(area);
+      System.out.println("The area of the circle is :- "+area);
   }
 }
