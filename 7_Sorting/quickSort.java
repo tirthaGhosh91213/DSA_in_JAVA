1f15cf95380140af3a8c56e45cf7fa8ba9afebb1
Tirtha Ghosh <ghoshtirtha1234@gmail.com>
2025-07-03 23:18:11 +0530

    Modify

--- 7_Sorting/quickSort.java
+++ 7_Sorting/quickSort.java
@@ -1,10 +1,6 @@
 import java.util.Arrays;
 public class quickSort {
-  
-
-
-
-    public static void QuickSort(int[] arr, int low, int high) {
+ public static void QuickSort(int[] arr, int low, int high) {
         if (low < high) {
             int pi = partition(arr, low, high);  
             QuickSort(arr, low, pi - 1);        
@@ -16,23 +12,25 @@ public class quickSort {
         int pivot = arr[high]; 
         int i = low - 1;        
 
-        for (int j = low; j < high; j++) {
+        for (int j=low; j<high; j++) {
             if (arr[j] < pivot) {
                 i++;
                 
-                int temp = arr[i]; arr[i] = arr[j]; arr[j] = temp;
+                int temp= arr[i]; arr[i] = arr[j]; arr[j] = temp;
             }
         }
 
         
-        int temp = arr[i + 1]; arr[i + 1] = arr[high]; arr[high] = temp;
+        int temp= arr[i + 1]; arr[i + 1] = arr[high]; arr[high] = temp;
 
         return i + 1;
     }
 
     public static void main(String[] args) {
         int[] arr = {10, 7, 8, 9, 1, 5};
+
         QuickSort(arr, 0, arr.length - 1);
+
         System.out.println("Sorted array: " + Arrays.toString(arr));
     }
 }
