677da5cfc373db9c1ad7ada7302cb24cf5035e18
Tirtha Ghosh <ghoshtirtha1234@gmail.com>
2025-07-04 12:51:30 +0530

    Modify code

--- 7_Sorting/countingInbuild.java
+++ 7_Sorting/countingInbuild.java
@@ -1,13 +1,15 @@
 
- import java.util.Arrays;
 public class countingInbuild{
   
 public static void countingSort(int[] arr) {
-    int max = Arrays.stream(arr).max().orElse(Integer.MIN_VALUE);
+    int max = Integer.MIN_VALUE;
+    for(int i=0;i<arr.length;i++){
+        max=Math.max(max, arr[i]);
+    }
 
         int[] count = new int[max + 1];
-        for (int num : arr) {
-            count[num]++;
+        for (int i=0;i<arr.length;i++) {
+            count[arr[i]]++;
         }
 
         int index = 0;
@@ -19,9 +21,6 @@ public static void countingSort(int[] arr) {
         }
     }
 
-  public static void inbuiltSort(int[] arr) {
-        Arrays.sort(arr); 
-    }
 
    public static void printArray(int[] arr) {
         for (int num : arr) {
@@ -31,21 +30,12 @@ public static void countingSort(int[] arr) {
     }
 
     public static void main(String[] args) {
-        // Counting Sort
         int[] countingArray = {4, 2, 2, 8, 3, 3, 1};
         System.out.println("Original Array for Counting Sort:");
         printArray(countingArray);
         countingSort(countingArray);
         System.out.println("Sorted Array (Counting Sort):");
         printArray(countingArray);
-
-        // Inbuilt Sort
-        int[] inbuiltArray = {64, 25, 12, 22, 11};
-        System.out.println("\nOriginal Array for Inbuilt Sort:");
-        printArray(inbuiltArray);
-        inbuiltSort(inbuiltArray);
-        System.out.println("Sorted Array (Inbuilt Sort):");
-        printArray(inbuiltArray);
     }
 }
 
