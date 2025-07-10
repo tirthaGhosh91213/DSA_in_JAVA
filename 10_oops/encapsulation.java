1a1c485b1e6c7709fc11a2be79dfb0e9f8305a4e
Tirtha Ghosh <ghoshtirtha1234@gmail.com>
2025-07-10 23:59:55 +0530

    Update encapsulation.java

--- 10_oops/encapsulation.java
+++ 10_oops/encapsulation.java
@@ -4,6 +4,6 @@ public class encapsulation {
   public static void main(String[] args) {
     int num=7;
     String jercyNo= "02";
-    System.out.println("Hey tirtha "+num+" Your number is "+jercyNo);
+    System.out.println("Hey tirtha "+num+" Your Number is "+jercyNo);
   }
 }
