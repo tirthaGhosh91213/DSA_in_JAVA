ae5d6a390d3cfff78a7d6bffb3c2a78cdeb6dd97
Tirtha Ghosh <ghoshtirtha1234@gmail.com>
2025-07-11 23:17:51 +0530

    Modify

--- 10_oops/inheritance.java
+++ 10_oops/inheritance.java
@@ -1,20 +1,20 @@
 public class inheritance {
 
-    // Base class (level 1)
+  
     class Animal {
         void eat() {
             System.out.println("Animal is eating...");
         }
     }
 
-    // Derived class (level 2)
+    
     class Dog extends Animal {
         void bark() {
             System.out.println("Dog is barking...");
         }
     }
 
-    // Derived class (level 3)
+   
     class Puppy extends Dog {
         void weep() {
             System.out.println("Puppy is weeping...");
