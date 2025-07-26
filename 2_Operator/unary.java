9f0a4adf74def8d078a8aff3da100c57d75bf827
Tirtha Ghosh <ghoshtirtha1234@gmail.com>
2025-07-26 22:12:10 +0530

    Update unary.java

--- 2_Operator/unary.java
+++ 2_Operator/unary.java
@@ -7,7 +7,7 @@ public class unary{
 
     int a=10;
       int b=++a;
-      System.out.println("The original number = "+a); //11
-      System.out.println("The updated number = "+b); //11
+      System.out.println("The original number : "+a); //11
+      System.out.println("The updated number :"+b); //11
   }
 }
