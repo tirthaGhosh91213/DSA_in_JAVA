2c8f9d0548916f6f0bfa0823b109ed9312577ff2
Tirtha Ghosh <ghoshtirtha1234@gmail.com>
2025-08-11 23:39:44 +0530

    Update areaOfCircle.java

--- 1_varibleAndDataType/areaOfCircle.java
+++ 1_varibleAndDataType/areaOfCircle.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class areaOfCircle{
   public static void main(String[] args) {
-      System.out.println("Enter the radi ous of the circle =");
+      System.out.println("Enter the radious of the circle =");
       Scanner sc=new Scanner(System.in);
       float rad=sc.nextFloat();
       float area =3.14f*rad*rad;
