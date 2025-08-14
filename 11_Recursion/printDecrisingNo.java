9a1f4bf5eb5d015bafc8d971bd45db27483fa852
Tirtha Ghosh <ghoshtirtha1234@gmail.com>
2025-08-14 21:46:18 +0530

    solve  some question

--- 11_Recursion/printDecrisingNo.java
+++ 11_Recursion/printDecrisingNo.java
@@ -9,9 +9,32 @@ public class printDecrisingNo {
     System.out.print(n+" ");
     PrintDec(n-1);
   }
-  public static void main(String[] args) {
-    int n=10;
-    PrintDec(n);
+  public static  void PrintInc(int n){
+    if(n==1){
+      System.out.print(1+" ");
+      return;
+    }
+    PrintInc(n-1);
+    System.out.print(n+" ");
+    
+  }
+  public static  int  fact(int n){
+    int factorial;
+    if(n==0){
+      
+      return  1;
+    }
 
+   factorial= n*fact(n-1);
+   return factorial;
+    
+  }
+  public static void main(String[] args) {
+    int n=8;
+    PrintDec(n);
+    PrintInc(n);
+    int result=fact(n);
+    System.out.println("\n");
+    System.out.println(result);
   }
 }
