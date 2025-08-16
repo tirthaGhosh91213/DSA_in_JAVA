88007bdc99d0849b5c5df373b7ca00deab04f8df
Tirtha Ghosh <ghoshtirtha1234@gmail.com>
2025-08-16 21:43:32 +0530

    Update charPattern.java

--- 5_Patterns/charPattern.java
+++ 5_Patterns/charPattern.java
@@ -4,7 +4,7 @@ public class charPattern{
       char ch='A';
       for(int i=1;i<=n;i++){
         for(int j=1;j<=i;j++){
-          System.out.print(" "+ch);
+          System.out.print( " "+ch);
           ch++;
         }
         System.err.println();
