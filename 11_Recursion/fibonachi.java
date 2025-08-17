72926a1bbd65ca92f8565f4766e66d4d47769480
Tirtha Ghosh <ghoshtirtha1234@gmail.com>
2025-08-17 23:49:52 +0530

    array sorted or not

--- 11_Recursion/fibonachi.java
+++ 11_Recursion/fibonachi.java
@@ -1,3 +1,5 @@
+
+
 public class fibonachi {
   public static int fibo(int n){
     if(n==0 || n==1){
@@ -6,9 +8,19 @@ public class fibonachi {
     int fib=fibo(n-1)+fibo(n-2);
     return fib;
   }
+  public static boolean isShorted(int arr[], int i){
+    if(i== arr.length-1){
+      return true;
+    }
+    if(arr[i]<arr[i+1]){
+      return isShorted(arr, i+1);
+    }
+    return false;
+  }
   public static void main(String[] args) {
-    int n=6;
-    int result=fibo(n);
+    // int n=6;
+    int arr[]={1,2,4,5};
+    boolean result=isShorted(arr,0);
     System.out.println(result);
   }
 }
