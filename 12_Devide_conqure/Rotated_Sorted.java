b9387d17aacf4ec95ea92b69af154b71aaf4d60e
Tirtha Ghosh <ghoshtirtha1234@gmail.com>
2025-08-22 21:25:41 +0530

    search in a rotated array

---
+++ 12_Devide_conqure/Rotated_Sorted.java
@@ -0,0 +1,31 @@
+public class Rotated_Sorted {
+  public static int search(int arr[],int target,int si,int ei){
+    if(si>ei){
+      return -1;
+    }
+    int mid=si+(ei-si)/2;
+    if(arr[mid]==target){
+      return mid;
+    }
+    if(arr[si]<=arr[mid]){
+      if(target>=arr[si]&&target<=arr[mid]){
+        return search(arr, target, si, mid-1);
+      }else{
+        return search(arr, target,mid+1, ei);
+      }
+    }
+    else{
+      if(target<=arr[ei]&&target>=arr[mid]){
+        return search(arr, target, mid+1, ei);
+      }else{
+        return search(arr, target, si, mid-1);
+      }
+    }
+  }
+  public static void main(String[] args) {
+    int arr[]= {4, 5, 6, 7, 0, 1, 2};
+    int target=6;
+    int getIdx=search(arr, target, 0, arr.length-1);
+    System.out.println(getIdx);
+  }
+}
