a8cc00f98078cd6ad768f7a934eded9ca94c93d9
Tirtha Ghosh <ghoshtirtha1234@gmail.com>
2025-09-26 21:33:39 +0530

    solving the max value from array list and reverce the array list

--- 14_Array_List/OperationList.java
+++ 14_Array_List/OperationList.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class OperationList {
   public static void main(String[] args) {
     ArrayList<Integer> list=new ArrayList<>();
-    ArrayList<Boolean> list2=new ArrayList<>();
+    // ArrayList<Boolean> list2=new ArrayList<>();
 
     // add operation ... TC = 0(1)
 
---
+++ 14_Array_List/ReverseArrayList.java
@@ -0,0 +1,30 @@
+
+import java.util.ArrayList;
+
+
+public class ReverseArrayList {
+  public static void main(String[] args) {
+    ArrayList<Integer> list = new ArrayList<>();
+    list.add(1);
+    list.add(2);
+    list.add(3);
+    list.add(4);
+    list.add(5);
+  //   for(int i=list.size()-1;i>=0;i--){
+  //     System.out.print(list.get(i)+" ");
+  //   }
+  //   System.out.println();
+  // }
+
+  int max = Integer.MIN_VALUE;
+
+  for(int i=0; i<list.size();i++){
+    if(max < list.get(i)){
+      max=list.get(i);
+    
+    }
+   
+  }
+   System.out.println("The max value is "+max);
+}
+}
