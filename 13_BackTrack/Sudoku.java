77dd74ae758d951673bfa3173384173350d75ab6
Tirtha Ghosh <ghoshtirtha1234@gmail.com>
2025-09-26 11:08:09 +0530

    solving sudoku problem

--- 13_BackTrack/Sudoku.java
+++ 13_BackTrack/Sudoku.java
@@ -1,4 +1,59 @@
 public class Sudoku {
+  public static  boolean isSafe(int sudoku[][], int row , int col, int digit){
+    for (int i=0;i<=8;i++){
+      if(sudoku[i][col]==0){
+        return false;
+      }
+    }
+    for (int j=0;j<=8;j++){
+      if(sudoku[row][j]==0){
+        return false;
+      }
+    }
+    int sr=(row/3)*3;
+    int sd=(col/3)*3;
+    for(int i=sr;i<sr+3;i++){
+      for(int j=sd;j<sd+3;j++){
+        if(sudoku[i][j]==0){
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+  public static boolean sudokusolver(int sudoku[][] , int row, int col){
+    if(row==9){
+      return true;
+    }
+    int nextRow = row, nextCol=col+1;
+    if(col+1==9){
+      nextRow=row+1;
+      nextCol=0;
+    }
+    if(sudoku[row][col] !=0){
+      return sudokusolver(sudoku, nextRow, nextCol);
+    }
+    for(int digit =0;digit <=9;digit++){
+      if(isSafe(sudoku,row,col,digit)){
+        sudoku[row][col]=digit;
+        if(sudokusolver(sudoku, nextRow, nextCol)){
+          return true;
+        }
+        sudoku[row][col]=0;
+
+      }
+    }
+return false;
+  }
+  public static void printSudoku(int sudoku[][]){
+    for(int i=0;i<9;i++){
+      for(int j=0;j<9;j++){
+       System.out.println(sudoku[i][j]+" ");
+      }
+      System.out.println();
+    }
+
+  }
   public static void main(String[] args) {
     int sudoku[][]=
 {
@@ -12,5 +67,16 @@ public class Sudoku {
 {0,3,0,0,7,2,0,0,4},
 {0,4,9,0,3,0,0,5,7},
 {8,2,7,0,0,9,0,1,3}};
+
+if(sudokusolver(sudoku, 0, 0)){
+    System.out.println("Solutin is exist ");
+    printSudoku(sudoku);  
   }
+  else{
+    System.out.println("Solution dose not exist ");
+  }
+  }
+
+
+
 }
