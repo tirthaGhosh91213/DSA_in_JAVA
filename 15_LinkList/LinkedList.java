da3d877c90be42dbc3c1e600d5307c4db2a476ae
Tirtha Ghosh <ghoshtirtha1234@gmail.com>
2025-11-19 19:52:30 +0530

    Operations of linkedlist

---
+++ 15_LinkList/LinkedList.java
@@ -0,0 +1,56 @@
+
+public class LinkedList {
+  public static class Node{
+    int data ;
+    Node next;
+    public Node(int data ){
+      this.data=data;
+      this.next=null;
+    }
+  }
+  public static Node head;
+  public static Node tail;
+  public  void addFirst(int data ){
+    Node newNode=new Node(data);
+    if(head==null){
+      head=tail=newNode;
+      return;
+    }
+    newNode.next=head;
+    head=newNode;
+
+  }
+  public void addLast(int data){
+    Node newNode= new Node(data);
+    if(head==null){
+      head=tail=newNode;
+
+    }
+    tail.next=newNode;
+    tail=newNode;
+  }
+  public void print(){
+    if(head==null){
+      System.out.println("LinkedList is empty");
+      return;
+    }
+    Node temp=head;
+    while(temp!=null){
+      System.out.print(temp.data+"-> ");
+      temp=temp.next;
+    }
+    System.out.println("null");
+  }
+  public static void main(String[] args) {
+    LinkedList ll=new LinkedList();
+    ll.print();
+    ll.addFirst(1);
+    ll.print();
+    ll.addFirst(2);
+    ll.print();
+    ll.addLast(3);
+    ll.print();
+    ll.addLast(4);
+    ll.print();
+  }
+}
