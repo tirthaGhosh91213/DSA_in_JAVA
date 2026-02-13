41327706c86f5da1530a62e1b3217937dbf989e5
Tirtha Ghosh <ghoshtirtha1234@gmail.com>
2026-02-13 20:28:44 +0530

    revesion array

--- 6_Array/basic2.java
+++ 6_Array/basic2.java
@@ -27,17 +27,20 @@ public class basic2 {
         System.out.println("The max value is " + min);
     }
 public static void subArray(int arr[]){
+    int tp=0;
     for(int i=0;i<arr.length;i++){
         for(int j=i;j<arr.length;j++){
             for(int k=i;k<j;k++){
                 System.out.print(arr[k]+"");
             }
              System.out.println();
+             tp++;
             
         }
         System.out.println();
+       
     }
-   
+    System.out.println("number os sub array :-"+tp);
 }
     public static void pairArray(int arr[]){
         for(int i=0;i<arr.length;i++){
