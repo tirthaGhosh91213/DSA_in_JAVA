c0fa111e9fdbd279bee4e8675185e3b53fc55aed
Tirtha Ghosh <ghoshtirtha1234@gmail.com>
2026-02-19 00:02:07 +0530

    Revise Bubble short

--- 8_AdvanceSorting/bubbleSelectionInser.java
+++ 8_AdvanceSorting/bubbleSelectionInser.java
@@ -1,14 +1,12 @@
 
 public class bubbleSelectionInser {
-  
-
 
     public static void bubbleSort(int[] arr) {
         int n = arr.length;
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
-                   
+
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
@@ -17,13 +15,27 @@ public class bubbleSelectionInser {
         }
     }
 
+    public static void BubbleShort2(int arr[]) {
+
+        for (int turn = 0; turn < arr.length - 1; turn++) {
+            for (int trackTurn = 0; trackTurn < arr.length - 1 - turn; trackTurn++) {
+                if (arr[trackTurn] > arr[trackTurn + 1]) {
+                    int temp = arr[trackTurn];
+                    arr[trackTurn] = arr[trackTurn+1];
+                    arr[trackTurn+1] = temp;
+                }
+            }
+
+        }
+
+    }
+
     public static void insertionSort(int[] arr) {
         int n = arr.length;
         for (int i = 1; i < n; i++) {
             int key = arr[i];
             int j = i - 1;
 
-           
             while (j >= 0 && arr[j] > key) {
                 arr[j + 1] = arr[j];
                 j = j - 1;
@@ -32,61 +44,64 @@ public class bubbleSelectionInser {
         }
     }
 
-    
     public static void selectionSort(int[] arr) {
         int n = arr.length;
         for (int i = 0; i < n - 1; i++) {
             int minIndex = i;
 
-            
             for (int j = i + 1; j < n; j++) {
                 if (arr[j] < arr[minIndex]) {
                     minIndex = j;
                 }
             }
 
-            
             int temp = arr[minIndex];
             arr[minIndex] = arr[i];
             arr[i] = temp;
         }
     }
 
-   
     public static void printArray(int[] arr) {
-        for (int num : arr) {
-            System.out.print(num + " ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
 
-    
     public static void main(String[] args) {
+        int[] arr = {100, 50, 64, 76, 92};
         int[] bubbleArray = {64, 34, 25, 12, 22, 11, 90};
         int[] insertionArray = {12, 11, 13, 5, 6};
         int[] selectionArray = {64, 25, 12, 22, 11};
 
         // Bubble Sort
+        /*
         System.out.println("Original Array for Bubble Sort:");
         printArray(bubbleArray);
         bubbleSort(bubbleArray);
         System.out.println("Sorted Array (Bubble Sort):");
         printArray(bubbleArray);
-
+        BubbleShort2(arr);
+        printArray(arr);
+ */
+BubbleShort2(arr);
+        printArray(arr);
         // Insertion Sort
+        /*
         System.out.println("\nOriginal Array for Insertion Sort=");
         printArray(insertionArray);
         insertionSort(insertionArray);
         System.out.println("Sorted Array (Insertion Sort)=");
         printArray(insertionArray);
-
+ */
         // Selection Sort
+        /*
         System.out.println("\nOriginal Array for Selection Sort=");
         printArray(selectionArray);
         selectionSort(selectionArray);
         System.out.println("Sorted Array (Selection Sort)=");
         printArray(selectionArray);
+         */
     }
-
 
 }
