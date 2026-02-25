3ae4bf55d108ca5fff8e30d8cdd53c777d02f627
Tirtha Ghosh <ghoshtirtha1234@gmail.com>
2026-02-25 20:40:31 +0530

    revise the recursion

--- 11_Recursion/binaryString.java
+++ 11_Recursion/binaryString.java
@@ -9,7 +9,25 @@ public class binaryString {
       binaryString(n-1, 1, str+"1");
     }
   }
+  public static void printDec(int n){
+    if(n==1){
+      System.out.println(n);
+      return;
+    }
+    System.out.print(n+" ");
+    printDec(n-1);
+  }
+   public static void printInc(int n){
+    if(n==1){
+      System.out.print(n+" ");
+      return;
+    }
+    printInc(n-1);
+    System.out.print(n+" ");
+    
+  }
   public static void main(String[] args) {
-    binaryString(3, 0, "");
+    // binaryString(3, 0, "");
+    printInc(10);
   }
 }
